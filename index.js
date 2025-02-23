525749b8ad54cd92d51aa535240ecedde70c1891
JaneOcloo <jane.ocloo1@gmail.com>
2025-02-23 12:04:09 +0300

    included api and updated js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 alert("Hello👻");
-
-function generatePoem(event) {
-  event.preventDefault();
-  let poemElement = document.querySelector("#content");
-
+function displayPoem(response) {
   new Typewriter("#content", {
-    strings: "The poem💕👣💕",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let UserInstructions = document.querySelector("#user-instructions");
+
+  let poemElement = document.querySelector("#content");
+
+  let apiKey = "eafo3c54a06d9f77ba3t96121805c98f";
+  let prompt = `Write a very beautiful, heart warming and knee weakening about ${UserInstructions.value} love poem using ideas form Reddit's UnsentLetters subreddits`;
+  let context =
+    "You are an avid  romantic poet on Reddit's UnsentLettter subreddit. Make sure to give a very brief, human like and emotion evoking piece ";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
